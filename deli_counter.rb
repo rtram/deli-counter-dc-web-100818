9fcc863413d28356833bd14542a2d9769be5119a
Robin Tram <robin.tram@gmail.com>
2018-09-19 16:10:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+
+def line (array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  elsif array.length > 0 
+    new_array = []
+    n = 0
+    while n < array.length
+      new_array.push(" #{n + 1}. #{array[n]}")
+      n = n + 1
+    end 
+    puts "The line is currently: #{new_array}"
+  end 
+end 
